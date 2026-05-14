6a0c1febe56cfc3c43ad9d0a629ce6126ec36454
a.isaeva <isaeva.nastya04@gmail.com>
2026-05-14 19:10:45 +0300

    chore: setup database connection

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm'
 @Module({
-  imports: [],
+  imports: [ ConfigModule.forRoot({
+    isGlobal: true
+  }), 
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule], 
+    inject: [ConfigService], 
+    useFactory:(configService: ConfigService)=>{
+      return{
+        type: 'postgres', 
+        host: configService.get('DB_HOST'), 
+        port: +(configService.get('DB_PORT')), 
+        username: configService.get('DB_USER'), 
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        synchronize: true,
+        autoLoadEntities: true,
+      }
+    }
+  })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -4,6 +4,6 @@ import 'dotenv/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.BACKEND_PORT || 3000);
 }
 bootstrap();

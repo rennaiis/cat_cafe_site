0c6a4cef5abf6440114904a3c9a608ef90b6ce28
a.isaeva <isaeva.nastya04@gmail.com>
2026-05-15 20:32:59 +0300

    fix: better styles for header

--- app/frontend/src/components/Header.tsx
+++ app/frontend/src/components/Header.tsx
@@ -1,5 +1,4 @@
 import '../index.css'
-import backImage from '../assets/header-back.png'
 import logo from '../assets/logo_no_background.png'
 import left_paw from '../assets/long-paw-1.png'
 import right_paw from '../assets/long-paw-2.png'
@@ -8,22 +7,23 @@ import { NavLink } from 'react-router-dom'
 const LandingData = {
     cafe_name: 'Котокафе КОфеТерий'
 }
-
 function Header(){
     return (
+        
         <header>
             <div className='header-line'>
                 <img src={logo} alt="logo" className='logo'/>
                 <h1 className='header'>{LandingData.cafe_name}</h1>
             </div>
             <div className='menu-line'>
-                <img src={left_paw} alt="" className='nav-paw'/>
+                {/* <img src={left_paw} alt="" className='nav-paw'/> */}
                 <nav className='inline-flex'>
                     <button><NavLink to={'/'}>О нас</NavLink></button>
                     <button><NavLink to={'/cats'}>Наши котики</NavLink></button>
                     <button><NavLink to={'/gallery'}>Галерея</NavLink></button>
                 </nav>
-                <img src={right_paw} alt="" className='nav-paw'/>
+                {/* <img src={right_paw} alt="" className='nav-paw'/> */}
+                <div className="header-bg-overlay"></div>
             </div>
         </header>
     )
--- app/frontend/src/pages/Landing.tsx
+++ app/frontend/src/pages/Landing.tsx
@@ -1,10 +1,9 @@
 import '../index.css'
-
+import backImage from '../assets/header-back.png'
 function Landing(){
+
     return (
-        <>
-        главная страничка
-        </>
+        <>main</>
     )
 }
 

dc19e24b574f4cb87c5fcca801cf8c2917bc611a
a.isaeva <isaeva.nastya04@gmail.com>
2026-05-19 13:40:40 +0300

    refactor: delete unnecesary resource

--- app/backend/src/photos_applications/dto/update-photos_application.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePhotosApplicationDto } from './create-photos_application.dto';
-
-export class UpdatePhotosApplicationDto extends PartialType(CreatePhotosApplicationDto) {}
--- app/backend/src/photos_applications/photos_applications.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PhotosApplicationsService } from './photos_applications.service';
-import { PhotosApplicationsController } from './photos_applications.controller';
-
-@Module({
-  controllers: [PhotosApplicationsController],
-  providers: [PhotosApplicationsService],
-})
-export class PhotosApplicationsModule {}

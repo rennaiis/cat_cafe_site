a97cfcff3190ddf9a4d305ba9aebf03f76564f2d
a.isaeva <isaeva.nastya04@gmail.com>
2026-06-10 01:03:56 +0300

    feat: create API functions for rules

---
+++ app/frontend/src/API/RulesAPI.ts
@@ -0,0 +1,39 @@
+import type { Rule } from "../types";
+
+const URL = "http://localhost:3000/rules"
+
+export async function getRules() {
+   const res =  await fetch(URL, {
+      credentials: 'include',
+   });
+   if (!res.ok) throw new Error(`can't get rules`)
+   return res.json()
+}
+
+export async function createRule(rule: Omit<Rule, 'id'>){
+    const res = await fetch(URL, {
+        credentials: 'include',
+        method: 'POST', 
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(rule)
+    })
+    return res.json()
+}
+
+export async function updateRule(rule:Omit<Rule, 'id'>, id: number) {
+    const res = await fetch(`${URL}/${id}`, {
+        credentials: 'include',
+        method: 'PATCH', 
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(rule)
+    })
+    return res.json()
+}
+
+export async function removeRule(id: number) {
+    const res=await fetch(`${URL}/${id}`, {
+        credentials: 'include',
+        method: 'DELETE'
+    })   
+    if (!res.ok) throw new Error(`can't delete rule ${id}`)
+}

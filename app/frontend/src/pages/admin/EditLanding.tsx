a23af4dbd13dd3deb3ae6513b47575dd05992213
a.isaeva <isaeva.nastya04@gmail.com>
2026-06-10 01:05:03 +0300

    feat: CRUD for rules on frontend

--- app/frontend/src/pages/admin/EditLanding.tsx
+++ app/frontend/src/pages/admin/EditLanding.tsx
@@ -1,32 +1,96 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, type ChangeEvent } from "react"
 import { LandingItemType } from "../../../../enums/LandingItemType"
 import type { LandingData, Rule } from "../../types"
-import { landingDataTest, rulesTest } from "../../test/testLandingData"
+import { rulesTest } from "../../test/testLandingData"
 import s from '../../styles/adminLanding.module.css'
 import { getLandingData, saveLandingData } from "../../API/LandingAPI"
+import { createRule, getRules, removeRule, updateRule } from "../../API/RulesAPI"
 const rulesList: Rule[] = rulesTest
+const optionsList: string[] = ['При входе', 'В котокафе','Посещение с детьми','Аллергия']
 
 function EditLanding() {
     const [landingData, setLandingData] =  useState<LandingData>()
-    useEffect(()=>{
+    const [editedRuleId, setEditedRuleId] = useState<number|null>(null)
+    const [rules, setRules] = useState<Rule[]>([])
+    const [editedRule, setEditedRule] = useState<Rule | null>(null)
+    const [ruleForm, setRuleForm] = useState<Omit<Rule, 'id'>>({
+        category: optionsList[0], 
+        text: ''
+    })
+    function loadData() {
         getLandingData().then((data)=>{
             setLandingData(data)
         }).catch((err)=>console.error('loading landing mistake: ', err))
-    }, [])
-    const handleChange = (key: LandingItemType, value: string | number) => {
+        getRules().then((data)=>{
+            setRules(data)
+        }).catch((err)=>console.error('loading rules mistake: ', err))
+    }
+
+    useEffect(()=> {loadData()}, [])
+
+    const handleChangeLanding = (key: LandingItemType, value: string | number) => {
         if (!landingData) return
         setLandingData({
             ...landingData,
             [key]: value
         })
     }
+    const handleFormChange = (e: ChangeEvent<HTMLSelectElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setRuleForm((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+    const handleEditChange = (e: ChangeEvent<HTMLSelectElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    if (!editedRule) return;
+        setEditedRule({
+            ...editedRule,
+            [name]: value
+        })
+    }
+
+    
+    async function addNewRule(e: React.FormEvent) {
+        e.preventDefault()
+        try{
+            if (ruleForm.category !== '' && ruleForm.text !== ''){
+            await createRule(ruleForm)
+            }
+            loadData()
+            console.log(ruleForm)
+            setRuleForm({
+                category: optionsList[0], 
+                text: ''
+            })
+        }catch(err){
+            console.error("create error ", err)
+        }
+        
+    }
+
+    async function editRule(e: React.FormEvent) {
+        e.preventDefault()
+        if (!editedRule) return
+        try{
+            await updateRule({
+                category: editedRule.category,
+                text: editedRule.text
+            }, editedRule.id)
+            setEditedRule(null); 
+            loadData();           
+            } catch (err) {
+                console.error("edit error", err);
+        }
+    }
+
     async function saveLanding (e: React.FormEvent){
         e.preventDefault()
         if (!landingData) return
         await saveLandingData(landingData) 
     } 
-    const [rules, setRules] = useState<Rule[]>(rulesList)
-    const [ruleForm, setRuleForm] = useState({ text: "", category: "При входе" })
+    
     if (!landingData) {
         return <div>Загрузка...</div>; 
     }
@@ -40,7 +104,7 @@ function EditLanding() {
                         <input 
                             type="text" 
                             value={landingData[LandingItemType.CAFE_NAME]} 
-                            onChange={(e)=>handleChange(LandingItemType.CAFE_NAME, e.target.value)} 
+                            onChange={(e)=>handleChangeLanding(LandingItemType.CAFE_NAME, e.target.value)} 
                         />
                     </div>
 
@@ -49,7 +113,7 @@ function EditLanding() {
                         <textarea 
                             value={landingData[LandingItemType.ABOUT_US]} 
                             rows={10}
-                            onChange={(e)=>handleChange(LandingItemType.ABOUT_US, e.target.value)}/>
+                            onChange={(e)=>handleChangeLanding(LandingItemType.ABOUT_US, e.target.value)}/>
                     </div>
 
                     <div>
@@ -57,7 +121,7 @@ function EditLanding() {
                         <input 
                             type="text"
                             value={landingData[LandingItemType.ADRESS]}
-                            onChange={(e)=>handleChange(LandingItemType.ADRESS, e.target.value)}
+                            onChange={(e)=>handleChangeLanding(LandingItemType.ADRESS, e.target.value)}
                         />
                     </div>
 
@@ -66,7 +130,7 @@ function EditLanding() {
                         <input 
                             type="tel"
                             value={landingData[LandingItemType.CONTACT_PHONE]}
-                            onChange={(e)=>handleChange(LandingItemType.CONTACT_PHONE, e.target.value)}
+                            onChange={(e)=>handleChangeLanding(LandingItemType.CONTACT_PHONE, e.target.value)}
                         />
                     </div>
 
@@ -75,7 +139,7 @@ function EditLanding() {
                         <input 
                             type="email" 
                             value={landingData[LandingItemType.CONTACT_EMAIL]}  
-                            onChange={(e)=>handleChange(LandingItemType.CONTACT_EMAIL, e.target.value)}
+                            onChange={(e)=>handleChangeLanding(LandingItemType.CONTACT_EMAIL, e.target.value)}
                         />
                     </div>
 
@@ -84,7 +148,7 @@ function EditLanding() {
                         <input 
                             type="url" 
                             value={landingData[LandingItemType.VK_LINK]} 
-                            onChange={(e)=>handleChange(LandingItemType.VK_LINK, e.target.value)} />
+                            onChange={(e)=>handleChangeLanding(LandingItemType.VK_LINK, e.target.value)} />
                     </div>
 
                     <div>
@@ -92,7 +156,7 @@ function EditLanding() {
                         <input 
                             type="text" 
                             value={landingData[LandingItemType.MAP_LINK]} 
-                            onChange={(e)=>handleChange(LandingItemType.MAP_LINK, e.target.value)} 
+                            onChange={(e)=>handleChangeLanding(LandingItemType.MAP_LINK, e.target.value)} 
                         />
                     </div>
 
@@ -103,14 +167,14 @@ function EditLanding() {
                             <input 
                                 type="number" 
                                 value={Number(landingData[LandingItemType.CATS_IN_CAFE])}  
-                                onChange={(e)=>handleChange(LandingItemType.CATS_IN_CAFE, e.target.value)}/>
+                                onChange={(e)=>handleChangeLanding(LandingItemType.CATS_IN_CAFE, e.target.value)}/>
                         </div>
                         <div>
                             <label>Котиков нашли дом:</label>
                             <input 
                                 type="number" 
                                 value={Number(landingData[LandingItemType.CATS_AT_HOME])}  
-                                onChange={(e)=>handleChange(LandingItemType.CATS_AT_HOME, e.target.value)}/>
+                                onChange={(e)=>handleChangeLanding(LandingItemType.CATS_AT_HOME, e.target.value)}/>
                         </div>
                     </fieldset>
 
@@ -121,7 +185,7 @@ function EditLanding() {
                             <input
                                 type="number" 
                                 value={Number(landingData[LandingItemType.FIRST_HOUR_PRICE_STANDART])} 
-                                onChange={(e)=>handleChange(LandingItemType.FIRST_HOUR_PRICE_STANDART, e.target.value)}
+                                onChange={(e)=>handleChangeLanding(LandingItemType.FIRST_HOUR_PRICE_STANDART, e.target.value)}
                             />
                         </div>
                         <div>
@@ -129,7 +193,7 @@ function EditLanding() {
                             <input 
                                 type="number" 
                                 value={Number(landingData[LandingItemType.FOLLOWING_HOURS_PRICE_STANDART])}  
-                                onChange={(e)=>handleChange(LandingItemType.FOLLOWING_HOURS_PRICE_STANDART, e.target.value)}
+                                onChange={(e)=>handleChangeLanding(LandingItemType.FOLLOWING_HOURS_PRICE_STANDART, e.target.value)}
                             />
                         </div>
                         <div>
@@ -137,7 +201,7 @@ function EditLanding() {
                             <input
                                 type="number" 
                                 value={Number(landingData[LandingItemType.STUDENTS_PRICE])}  
-                                onChange={(e)=>handleChange(LandingItemType.STUDENTS_PRICE, e.target.value)}
+                                onChange={(e)=>handleChangeLanding(LandingItemType.STUDENTS_PRICE, e.target.value)}
                             />
                         </div>
                         <div>
@@ -145,7 +209,7 @@ function EditLanding() {
                             <textarea 
                                 value={landingData[LandingItemType.STUDENTS_CONDITIONS]}
                                 rows={2} 
-                                onChange={(e)=>handleChange(LandingItemType.STUDENTS_CONDITIONS, e.target.value)}
+                                onChange={(e)=>handleChangeLanding(LandingItemType.STUDENTS_CONDITIONS, e.target.value)}
                             />
                         </div>
                         <div>
@@ -153,7 +217,7 @@ function EditLanding() {
                             <input 
                                 type="number" 
                                 value={Number(landingData[LandingItemType.GROUP_DISCOUNT])} 
-                                onChange={(e)=>handleChange(LandingItemType.GROUP_DISCOUNT, e.target.value)} 
+                                onChange={(e)=>handleChangeLanding(LandingItemType.GROUP_DISCOUNT, e.target.value)} 
                             />
                         </div>
                         <div>
@@ -161,7 +225,7 @@ function EditLanding() {
                             <input 
                                 type="number" 
                                 value={Number(landingData[LandingItemType.GROUP_PEOPLE_AMOUNT])}  
-                                onChange={(e)=>handleChange(LandingItemType.GROUP_PEOPLE_AMOUNT, e.target.value)}
+                                onChange={(e)=>handleChangeLanding(LandingItemType.GROUP_PEOPLE_AMOUNT, e.target.value)}
                             />
                         </div>
                         <div>
@@ -169,7 +233,7 @@ function EditLanding() {
                             <input 
                                 type="text" 
                                 value={landingData[LandingItemType.GROUP_CONDITIONS]} 
-                                onChange={(e)=>handleChange(LandingItemType.GROUP_CONDITIONS, e.target.value)} 
+                                onChange={(e)=>handleChangeLanding(LandingItemType.GROUP_CONDITIONS, e.target.value)} 
                             />
                         </div>
                     </fieldset>
@@ -181,29 +245,36 @@ function EditLanding() {
             <section>
                 <h2>Управление правилами</h2>
 
-                <form onSubmit={(e) => e.preventDefault()}>
+                <form onSubmit={addNewRule}>
                     <h3>Добавить новое правило</h3>
                     <div>
                         <label>Категория:</label>
-                        <select value={ruleForm.category}>
-                            <option value="При входе">При входе</option>
-                            <option value="В котокафе">В котокафе</option>
-                            <option value="Посещение с детьми">Посещение с детьми</option>
-                            <option value="Аллергия">Аллергия</option>
+                        <select
+                            name='category'
+                            value={ruleForm.category}
+                            onChange={handleFormChange}>
+                            {optionsList.map((option)=>(
+                                <option key={option} value={option}>{option}</option>                            
+                            ))}
                         </select>
                     </div>
                     <div>
                         <label>Текст правила:</label>
-                        <textarea value={ruleForm.text} readOnly rows={3} placeholder="Введите текст правила..." />
+                        <textarea 
+                            name="text"
+                            value={ruleForm.text} 
+                            onChange={handleFormChange} 
+                            rows={3} 
+                            placeholder="Введите текст правила..." 
+                        />
                     </div>
-                    <button type="button">Добавить правило</button>
+                    <button type='submit'>Добавить правило</button>
                 </form>
 
                 <h3>Список текущих правил</h3>
                 <table>
                     <thead>
                         <tr>
-                            <th>ID</th>
                             <th>Категория</th>
                             <th>Текст правила</th>
                             <th>Действия</th>
@@ -212,18 +283,50 @@ function EditLanding() {
                     <tbody>
                         {rules.map((rule) => (
                             <tr key={rule.id}>
-                                <td>{rule.id}</td>
-                                <td>
-                                    <input type="text" value={rule.category} readOnly />
-                                </td>
-                                <td>
-                                    <textarea value={rule.text} readOnly rows={2} />
-                                </td>
-                                <td className={s.row}>
-                                    <button type="button">Сохранить строку</button>
-                                    <button type="button">Удалить</button>
-                                </td>
-                            </tr>
+                                {editedRule && editedRule.id === rule.id ? 
+                                <>
+                                    <td>
+                                    <select 
+                                        name="category"
+                                        value={editedRule.category}
+                                        onChange={handleEditChange}
+                                        >
+                                        {optionsList.map((option)=>(
+                                            <option value={option}>{option}</option>                            
+                                        ))}
+                                    </select>
+                                    </td>
+                                    <td>
+                                        <textarea
+                                        name="text"
+                                        value={editedRule.text} 
+                                        rows={2}
+                                        onChange={handleEditChange} />
+                                    </td>
+                                    <td className={s.row}>
+                                        <button onClick={()=>{setEditedRule(null) 
+                                        }
+                                        }>Отмена</button>
+                                        <button onClick={editRule}>Сохранить</button>
+                                    </td>
+                                </> : 
+                                <>
+                                    <td>{rule.category}</td>
+                                    <td>{rule.text}</td>
+                                    <td className={s.row}>
+                                        <button onClick={()=>setEditedRule({...rule})}>Редактировать</button>
+                                        <button 
+                                            type="button" 
+                                            onClick={async ()=>{
+                                                await removeRule(rule.id); 
+                                                loadData()
+                                            }}>Удалить
+                                        </button>
+                                    </td>
+                                </>
+                                }
+                            </tr> 
+                        
                         ))}
                     </tbody>
                 </table>

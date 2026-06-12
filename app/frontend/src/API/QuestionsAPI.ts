7f3b6312614c519f6910313a3af98457388aee2b
a.isaeva <isaeva.nastya04@gmail.com>
2026-06-12 12:30:58 +0300

    feat: API for anket questions

---
+++ app/frontend/src/API/QuestionsAPI.ts
@@ -0,0 +1,39 @@
+import type { Question} from "../types";
+
+const URL = "http://localhost:3000/questions"
+
+export async function getQuestions() {
+   const res =  await fetch(URL, {
+      credentials: 'include',
+   });
+   if (!res.ok) throw new Error(`can't get questions`)
+   return res.json()
+}
+
+export async function createQuestion(question: Omit<Question, 'id'>){
+    const res = await fetch(URL, {
+        credentials: 'include',
+        method: 'POST', 
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(question)
+    })
+    return res.json()
+}
+
+export async function updateQuestion(question:Omit<Question, 'id'>, id: number) {
+    const res = await fetch(`${URL}/${id}`, {
+        credentials: 'include',
+        method: 'PATCH', 
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(question)
+    })
+    return res.json()
+}
+
+export async function removeQuestion(id: number) {
+    const res = await fetch(`${URL}/${id}`, {
+        credentials: 'include',
+        method: 'DELETE'
+    })   
+    if (!res.ok) throw new Error(`can't delete question ${id}`)
+}

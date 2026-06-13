342a2ef09356f8020dbbc5836004ef13c850a82a
a.isaeva <isaeva.nastya04@gmail.com>
2026-06-13 16:38:28 +0300

    feat: add application first step

--- app/frontend/src/components/FillData.tsx
+++ app/frontend/src/components/FillData.tsx
@@ -1,17 +1,42 @@
 import { StatusType } from "../../../enums/StatusType"
-import { calculateAge, catsListTest } from "../test/testCatsList"
+import { catsListTest } from "../test/testCatsList"
 import { filesListTest } from "../test/testFiles"
-import type { Cat, MyFile } from "../types"
+import type { Adopter, Cat, MyFile } from "../types"
 import s from '../styles/catApplication.module.css'
 import { useState } from "react"
 
 const filesList: MyFile[] = filesListTest
 const catsList: Cat[] = catsListTest
 const catsInCafe: Cat[]  = catsList.filter(cat => cat.status.type === StatusType.IN_CAFE)
+interface FillDataProps {
+    onNext: (catId: number, adopterData: Omit<Adopter, 'id'>)=>void
+}
 
-function FillData(){
+function FillData({onNext}:FillDataProps){
     const [selectedCat, setSelectedCat] = useState<Cat | null>(null)
-
+    const [formData, setFormData] = useState<Omit<Adopter, 'id'>>({
+        first_name: '', 
+        last_name: '',
+        middle_name: '', 
+        email: '', 
+        mobile: '', 
+        contact: ''
+    })
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target
+        setFormData(prev=>({
+            ...prev, 
+            [name]: value
+        }))
+    }
+    const handleSubmit = (e: React.FormEvent)=>{
+        e.preventDefault()
+        if (!selectedCat){
+            alert('Пожалуйста, выберите котика')
+            return
+        }
+        onNext(selectedCat.id, formData)
+    }
     return (
         <>
         <main className="content-block">
@@ -30,34 +55,70 @@ function FillData(){
             ))}
             </div>
             <h3>Заполните данные о себе</h3>
-            <form className = {s.form} onSubmit={(e) => e.preventDefault()}>
+            <form className = {s.form} onSubmit={handleSubmit}>
                 <div>
-                    <label htmlFor="adopter-name" className="little-text">Имя</label>
-                    <input type="text" id='adopter-name' required/>
+                    <label htmlFor="first_name" className="little-text">Имя</label>
+                    <input 
+                        value={formData.first_name}
+                        type="text" 
+                        id='first_name' 
+                        name="first_name"
+                        onChange={handleInputChange} 
+                    required/>
                 </div>
                 <div>
-                    <label htmlFor="adopter-lastname" className="little-text">Фамилия</label>
-                    <input type="text" id='adopter-lastname' required/>
+                    <label htmlFor="last_name" className="little-text">Фамилия</label>
+                    <input 
+                        value={formData.last_name}
+                        type="text" 
+                        id='last_name' 
+                        name='last_name' 
+                        onChange={handleInputChange}
+                    required/>
                 </div>
                 <div>
-                    <label htmlFor="adopter-lastname" className="little-text">Отчество</label>
-                    <input type="text" id='adopter-middlename'/>
+                    <label htmlFor="middle_name" className="little-text">Отчество</label>
+                    <input 
+                        value={formData.middle_name}
+                        type="text" 
+                        id='middle_name'
+                        name="middle_name"
+                        onChange={handleInputChange} />
                 </div>
                 <div>
-                    <label htmlFor="adopter-email" className="little-text">Электронная почта</label>
-                    <input type="email" id='adopter-email' required/>
+                    <label htmlFor="email" className="little-text">Электронная почта</label>
+                    <input 
+                        value={formData.email}
+                        type="email"
+                        id='email' 
+                        name='email'
+                        onChange={handleInputChange} 
+                    required/>
                 </div>
 
                 <div>
-                    <label htmlFor="adopter-tel" className="little-text">Телефон (не обязательно)</label>
-                    <input type="tel" id='adopter-tel' placeholder="+7(000)111-11-11"  pattern="+7([0-9]{3})[0-9]{3}-[0-9]{2}-[0-9]{2}"/>
+                    <label htmlFor="mobile" className="little-text">Телефон (не обязательно)</label>
+                    <input
+                        value={formData.mobile} 
+                        type="tel" 
+                        id='mobile' 
+                        name='mobile'
+                        placeholder="+7(000)111-11-11" 
+                        pattern="+7([0-9]{3})[0-9]{3}-[0-9]{2}-[0-9]{2}"
+                        onChange={handleInputChange} />
                 </div>
                 <div>
-                    <label htmlFor="adopter-contact" className="little-text">Другой контакт (не обязательно) </label>
-                    <input type="text" id='adopter-contact'/>
+                    <label htmlFor="contact" className="little-text">Другой контакт (ВК, телеграм, не обязательно) </label>
+                    <input 
+                        value={formData.contact}
+                        type="text" 
+                        id='contact'
+                        name="contact"
+                        onChange={handleInputChange} 
+                    />
                 </div>              
 
-                <button className={s.submitButton}>Далее</button>
+                <button className={s.submitButton} type="submit">Далее</button>
             </form>
         </main>
         </>

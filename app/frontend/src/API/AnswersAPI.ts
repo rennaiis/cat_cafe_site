e911f7fec2dd47fc2cf854d60c63e85e48d86784
a.isaeva <isaeva.nastya04@gmail.com>
2026-06-15 19:37:00 +0300

    feat: API for answers

---
+++ app/frontend/src/API/AnswersAPI.ts
@@ -0,0 +1,39 @@
+import type { AnswerDTO } from "../types";
+
+const URL = "http://localhost:3000/answers"
+
+export async function getAnswers() {
+   const res =  await fetch(URL, {
+      credentials: 'include',
+   });
+   if (!res.ok) throw new Error(`can't get answers`)
+   return res.json()
+}
+
+export async function createAnswer(answer: AnswerDTO){
+    const res = await fetch(URL, {
+        credentials: 'include',
+        method: 'POST', 
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(answer)
+    })
+    return res.json()
+}
+
+export async function updateRule(answer:Omit<AnswerDTO, 'id'>, id: number) {
+    const res = await fetch(`${URL}/${id}`, {
+        credentials: 'include',
+        method: 'PATCH', 
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(answer)
+    })
+    return res.json()
+}
+
+export async function removeAnswer(id: number) {
+    const res=await fetch(`${URL}/${id}`, {
+        credentials: 'include',
+        method: 'DELETE'
+    })   
+    if (!res.ok) throw new Error(`can't delete answer ${id}`)
+}

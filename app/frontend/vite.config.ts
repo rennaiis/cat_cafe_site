a6859d78275388de527fa336965396a0f1a71af7
a.isaeva <isaeva.nastya04@gmail.com>
2026-06-15 14:55:20 +0300

    chore: system settings

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+  },
+  
   plugins: [react()],
 })
